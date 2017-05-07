d1052692b5adae178b6b4168ea2354ea34e002d1
Gleb Besarab <rmonsanin@gmail.com>
2017-05-07 14:12:11 +0300

    Add indexController with indexAction and tests for it

---
+++ src/test/java/rexxie/controllers/IndexControllerTest.java
@@ -0,0 +1,32 @@
+package rexxie.controllers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void indexActionCorrectResponseTest() {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/", String.class);
+        assertThat(response.getBody()).isEqualTo("Index action");
+    }
+
+    @Test
+    public void indexActionStatusCodeTest() {
+        ResponseEntity<String> response = this.restTemplate.getForEntity("/", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+}
